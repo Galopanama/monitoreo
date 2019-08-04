de4a585087fb0f9989e6d75babd007c22ac0098e
Gonzalo <gonzalogales@gmail.com>
2019-08-04 11:05:10 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+require_once ("DB.php");
+require_once ("model/Usuario.php");
+
+
+// TODO Cambiar a un prepared statement
+$sql = "select * from usuarios where login = '" . $_POST["inputEmail"] . "' and activo = true";
+$usuario = new Usuario();
+$usuario->load($_POST["inputEmail"]);
+$usuario->checkLogin($_POST["inputPassword"]);
+ 
+$db = new DB();
+$mysqli = $db->conecta();
+
+$result = $mysqli->query($sql);
+
+$usuario = $result->fetch_assoc();
+
+
+if (password_verify($_POST["inputPassword"], $usuario["password"])) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.';
+}
+
+
+$db->desconecta();
+
+?>
