3f4a74e0a91b156337827e89e151bad19c89033b
Galopanama <gonzalogales@gmail.com>
2019-08-18 13:41:00 +0200

    Se han creado los ficheros config_registrado.php, que comprueba si un usuario está logado o no, y el fichero config_admin.php, que además de comprobar si el usuario está logado, comprueba si tiene rango administrador y en caso contrario lo manda a la página inicial del usuario no administrador

---
+++ config/config_admin.php
@@ -0,0 +1,10 @@
+<?php
+// Este fichero debe actuar igual que config_registrado (de hecho debe hacer en parte lo mismo por lo que delegamos esa parte en el)
+require_once 'config_registrado.php';
+
+// Pero ademas, debe comprobar que el usuario que entra sea administrador
+if($_SESSION['tipo_de_usuario'] !== "administrador"){
+    // Devolvemos al usuario que no es administrador a su página inicial
+    header('Location: ' . _WEB_PATH_ . '/user/index.php');
+    exit;
+}
