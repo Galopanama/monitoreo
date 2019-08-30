777fdcd7d63adcf45ceac42b198eb41638f8990c
Galopanama <gonzalogales@gmail.com>
2019-08-30 16:32:52 +0200

    Creado un nuevo archivo autorizador.php encargado de dar o denegar el acceso a las páginas. Se han eliminado los archivos config_admin.php y config_registrado.php, pues se deriva la funcionalidad que ofrecían al archivo nuevo. Se modifican las llamadas a dichos ficheros

--- config/config_registrado.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-
-// Vamos a comprobar si el usuario está logado o no
-if (!isset($_SESSION['usuario_id']) || is_null($_SESSION['usuario_id']) || $_SESSION['usuario_id'] === ""){
-    header('Location: ' . _WEB_PATH_ . '/security/login.php');
-}
---
+++ security/autorizador.php
@@ -0,0 +1,19 @@
+<?php
+// Este fichero se encargará de permitir, o no, el acceso a la página en base al perfil del usuario y el acceso que exija cada página
+
+// Vamos a comprobar si el usuario está logado o no
+if (!isset($_SESSION['usuario_id']) || is_null($_SESSION['usuario_id']) || $_SESSION['usuario_id'] === ""){
+    header('Location: ' . _WEB_PATH_ . '/security/login.php');
+    exit;
+}
+
+if (!isset($perfiles_aceptados) || !is_array($perfiles_aceptados)){
+    $perfiles_aceptados = array();
+}
+
+if (!in_array($_SESSION['tipo_de_usuario'], $perfiles_aceptados)){
+    $_SESSION["error_message"] = "No tienes permiso para entrar a esta página";
+    // Si no tiene acceso a la página, lo redirigimos
+    header('Location: ' . _WEB_PATH_ . '/error.php');
+    exit;
+}
