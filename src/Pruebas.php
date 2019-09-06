7105732a229c14c2f1b284fb7691bf03ee672757
Gonzalo <gonzalogales@gmail.com>
2019-09-06 18:54:28 -0500

    continuan los retoques en las pruebas

--- src/Pruebas.php
+++ src/Pruebas.php
@@ -19,7 +19,7 @@ class Pruebas {
         }
         // Vamos a ordenar las más nuevas primero
         $sql .= " order by p.fecha desc ";
-        // Abrimos la conexion de la base de datos
+        // Abrimos la conexion de la base de prueba$prueba
         $db = new DB();
         // La siguiente llamada puede generar una excepción, que habrá que recoger en el método que la llame, o dejar que se propague
         $mysqli = $db->conecta();
@@ -50,7 +50,7 @@ class Pruebas {
                 $date = new DateTime($prueba['fecha']);
                 
                 $array_pruebas[] = new Prueba( // AQUI NO ESTOY SEGURO DE QUE ESTE NOMBRE DE OBJETO SEA CORRECTO
-                    $prueba['id_promotor'],
+                    $prueba['id_tecnologo'],
                     $prueba['id_persona_receptora'],
                     $date->format('d-m-Y'),
                     $prueba['consejeria_pre_prueba'],
@@ -61,19 +61,83 @@ class Pruebas {
             }
             // limpiamos los resultados de la memoria
             $result->free();
-            // por último desconectamos de la base de datos
+            // por último desconectamos de la base de prueba$prueba
             $stmt->close();
             $mysqli->close();
             // Devolvemos el array
             return $array_pruebas;
         }
         else {
-            // por último desconectamos de la base de datos
+            // por último desconectamos de la base de prueba$prueba
             $mysqli->close();
             throw new Exception("Error de BD: " . $mysqli->error);
         }
     }
 
+    public static function add($prueba, $db = null){
+        
+        // Si el objeto db no es nulo, estamos en una transacción
+        $transaccion = !is_null($db);
+
+        // Vamos a comprobar si este método forma parte de una transacción, para crear si no nuestro propio objeto de conexión a DB
+        if (!$transaccion){
+            // Abrimos la conexion de la base de prueba$prueba
+            $db = new DB();
+
+            // No controlamos la excepción a propósito, ya que al ser una llamada ajax, si mandamos a una página de error el usuario no notará nada
+            // Controlaremos la excepción en el php encargado de responder al ajax
+            $mysqli = $db->conecta();
+        }
+        else {
+            $mysqli = $db->getConexion();
+        }
+
+        // Errores será un array donde se guardarán los errores de validación del formulario, para después poder mostrarlas al usuario
+        // Es MUY IMPORTANTE que las claves del array sean los nombres de los campos que venían en el formulario, para poder informar al usuario
+        // posteriormente de cuales han sido los campos en los que se ha fallado
+        $errores = array();
+
+        // Ya hemos llegado al final de las validaciones. Si el array no está vacío, significa que han ocurrido errores, por tanto, lanzamos una excepción
+        if (sizeof($errores) > 0){
+            throw new ValidationException (serialize($errores));
+        }
+
+        $sql = "insert into " . Constantes::PRUEBA . " (id_tecnologo, id_persona_receptora, fecha,  (?, ?, now(), ?, ?, ?, ?)";
+
+        // Preparamos la sentencia anterior
+        if ($stmt = $mysqli->prepare($sql)) {
+            $fecha = "now()"; // Como ya sabemos, bind_param no permite cadenas o números directamente si no es con variables
+            //Enlazamos los parametros con los valores pasados, indicando ademas el tipo de cada uno
+            $stmt->bind_param('isiiii', 
+                $prueba['id_tecnologo'],
+                $prueba['id_persona_receptora'],
+                $prueba['consejeria_pre_prueba'],
+                $prueba['consejeria_post_prueba'],
+                $prueba['resultado_prueba'],
+                $prueba['realizacion_prueba']
+                );
+
+
+            // Ejecutamos la sentencia con los valores ya establecidos
+            if(!$stmt->execute()){
+                throw new Exception("Ocurrió un problema al introducir la prueba: " . $stmt->error);
+            }
+            
+            if ($transaccion) {
+                // Si estamos en una transacción, hay que terminarla aquí
+                $mysqli->commit();
+            }
+
+            $stmt->close();
+            $mysqli->close();
+        }
+        else {
+            throw new Exception("Error de BD: " . $mysqli->error);
+        }
+        
+    }
 }
+
+    class PruebaNotFoundException extends Exception {}
 
 ?>
--- user/prueba/addPrueba.php
+++ user/prueba/addPrueba.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/../../src/Pruebas.php';
 require_once __DIR__ . '/../../src/PersonasReceptoras.php';
 
 //$header = $smarty->fetch("header/.tpl");
+
 if (!empty($_POST['id_persona_receptora_buscada'])) {
     // Se han enviado datos
     $errores = array();
@@ -25,9 +26,7 @@ if (!empty($_POST['id_persona_receptora_buscada'])) {
         catch (PersonaReceptoraNotFoundException $e) {
             // Si no existe, el identificador no lo obtenemos del campo id_persona_receptora, sino de id_persona_receptora_buscada
             $datos_persona_receptora = [
-                'id_persona_receptora' => $_POST['id_persona_receptora_buscada'],
-                'poblacion' => $_POST['poblacion'],                                     // ESTE CAMPO Y EL SIGUIENTE SON DE OTRA TABLA
-                'poblacion_originaria' => $_POST['poblacion_originaria']?true:false     //PERO SE PODRIAN VER EN ESTA TABLA SI ES QUE EL USUARIO EXISTE??
+                'id_persona_receptora' => $_POST['id_persona_receptora_buscada']
             ];
             // Le pedimos al método que nos devuelva el objeto de la conexión, para poder hacer la transacción
             try {
@@ -41,35 +40,36 @@ if (!empty($_POST['id_persona_receptora_buscada'])) {
                 $errores[] = $e->getMessage();
             }
         }
-        // Si no han ocurrido errores hasta ahora, procedemos a intentar añadir la entrevista
+    }
+}
+    // Si no han ocurrido errores hasta ahora, procedemos a intentar añadir la entrevista
+    if (empty($errores)){
+        // Aunque en principio los nombres del formulario deberían de ser los mismos que los que espera la clase EntrevistaIndividual, 
+        // vamos a crear un array con dichas claves
+        $datos = [
+            'id_tecnologo' => $_SESSION['usuario_id'],
+            'id_persona_receptora' => $_POST['id_persona_receptora_buscada'], 
+            'consejeria_pre_prueba' => $_POST['consejeria_pre_prueba']?true:false,
+            'consejeria_post_prueba' => $_POST['consejeria_post_prueba']?true:false,
+            'resultado_prueba' => $_POST['resultado_prueba']?true:false,
+            'realizacion_prueba' => $_POST['realizacion_prueba']?true:false,
+        ];
+        try {
+            Entrevistas::add($datos, $db);
+        }
+        catch (ValidationException $e) {
+            // Los mensajes de validación vienen como un array serializado en el mensaje de la excepción
+            $errores = unserialize($e->getMessage());
+        }
+        catch (Exception $e) {
+            $errores[] = $e->getMessage();
+        }
         if (empty($errores)){
-            // Aunque en principio los nombres del formulario deberían de ser los mismos que los que espera la clase EntrevistaIndividual, 
-            // vamos a crear un array con dichas claves
-            $datos = [
-                'id_tecnologo' => $_SESSION['usuario_id'],
-                'id_persona_receptora' => $_POST['id_persona_receptora_buscada'], 
-                'consejeria_pre_prueba' => $_POST['consejeria_pre_prueba']?true:false,
-                'consejeria_post_prueba' => $_POST['consejeria_post_prueba']?true:false,
-                'resultado_prueba' => $_POST['resultado_prueba']?true:false,
-                'realizacion_prueba' => $_POST['realizacion_prueba']?true:false,
-            ];
-            try {
-                Entrevistas::add($datos, $db);
-            }
-            catch (ValidationException $e) {
-                // Los mensajes de validación vienen como un array serializado en el mensaje de la excepción
-                $errores = unserialize($e->getMessage());
-            }
-            catch (Exception $e) {
-                $errores[] = $e->getMessage();
-            }
-            if (empty($errores)){
-                // No ha habido errores, redirigimos a la página del listado
-                $_SESSION['exito_titulo'] = "Prueba añadida con éxito";
-                $_SESSION['exito_mensaje'] = "La prueba ha sido dada de alta correctamente en el sistema";
-                header('Location: ' . _WEB_PATH_ . '/user/prueba/index.php');
-                exit;
-            }
+        // No ha habido errores, redirigimos a la página del listado
+            $_SESSION['exito_titulo'] = "Prueba añadida con éxito";
+            $_SESSION['exito_mensaje'] = "La prueba ha sido dada de alta correctamente en el sistema";
+            header('Location: ' . _WEB_PATH_ . '/user/prueba/index.php');
+            exit;
         }
     }
     // Asignamos los errores si los hubiera (que si el código ha llegado aquí, los hay)
@@ -78,15 +78,19 @@ if (!empty($_POST['id_persona_receptora_buscada'])) {
     foreach (array_keys($_POST) as $elem) {
         $smarty->assign($elem, $_POST[$elem]);
     }
-}
+
 // El título de la página será Añadir prueba (la misma plantilla se utiliza para modificar)
 $smarty->assign('titulo', 'Añadir prueba');
-// // Necesitamos cargar un array con los tipos de usuario válidos. Dicho array está en la clase Usuarios
-// $smarty->assign('tipos_poblacion_permitidos', PersonasReceptoras::tipos_poblacion_permitidos); DUDA POR SI SE PUEDE O NO INTRODUCIR EN EL FORMULARIO
+
+// Necesitamos cargar un array con los tipos de usuario válidos. Dicho array está en la clase Usuarios
+$smarty->assign('tipos_poblacion_permitidos', PersonasReceptoras::tipos_poblacion_permitidos);
+
 // La variable main se utilizará en el archivo footer.php
 $main = $smarty->fetch("paginas/prueba/add_prueba.tpl");
+
 // Esta página necesita un javascript especial
 $footer = $smarty->fetch("footer/add_prueba.tpl");
+
 // Esta página necesita un javascript especial
 require_once '../../footer.php';
 $smarty->display('esqueleto_dashboard.html');
