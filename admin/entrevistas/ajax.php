f1839c8369e97779db164ccf2979d7e03ebe66a5
Galopanama <gonzalogales@gmail.com>
2019-09-08 20:28:52 +0200

    Creado el listado a partir del listado del usuario

--- admin/entrevistas/ajax.php
+++ admin/entrevistas/ajax.php
@@ -11,6 +11,9 @@ switch($_GET['funcion']){
     case "getAllIndividuales":
         echo json_encode(getAllIndividuales());
         exit;
+    case "getAllGrupales":
+        echo json_encode(getAllGrupales());
+        exit;
     case "buscar":
         echo json_encode(buscar_subreceptor($_GET['key']));
     default:
@@ -33,6 +36,32 @@ function getAllIndividuales() {
 
             $promotor = Usuarios::getUsuarioById($entrevistaIndividual->getId_promotor());
             $entrevistaIndividual->nombre_promotor = $promotor->getNombre() . ' ' . $promotor->getApellidos();
+        }
+
+        return prepara_para_json($lista);
+    }
+    catch (Exception $e) {
+        $array_response['error'] = 1;
+        $array_response['errorMessage'] = $e->getMessage();
+        return $array_response;
+    }
+}
+
+/**
+ * Devuelve todas las entrevistas grupales
+ */
+function getAllGrupales() {
+    try {
+        $lista = Entrevistas::getAllEntrevistasGrupales();
+
+        // Vamos a editar la lista, y añadir los datos de la persona receptora y el nombre del promotor
+        foreach($lista as $entrevista) {
+            $persona_receptora = PersonasReceptoras::getPersonaReceptora($entrevista->getId_persona_receptora());
+            $entrevista->poblacion = $persona_receptora->getPoblacion();      // esto se hace para añadir atributos a un objeto que no 
+            $entrevista->poblacion_originaria = $persona_receptora->getPoblacion_originaria();// son parte del objeto en si. Solo se hace
+                                                                                        // porque PHP lo permite. igual que Java 
+            $promotor = Usuarios::getUsuarioById($entrevista->getId_promotor());
+            $entrevista->nombre_promotor = $promotor->getNombre() . ' ' . $promotor->getApellidos();
         }
 
         return prepara_para_json($lista);
