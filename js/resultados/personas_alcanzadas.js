632fa20aca19c90d5da2dfc28ffa2a8b41327b7b
Gonzalo <gonzalogales@gmail.com>
2020-01-20 19:22:26 -0500

    fallan nombre_regiones y total_de_Per_Alc

--- js/resultados/personas_alcanzadas.js
+++ js/resultados/personas_alcanzadas.js
@@ -5,7 +5,7 @@
  */
 
 $(document).ready(function() {   
-    var respuesta = [] ;                                                                                                                              //# busca en el id del elemento html no de la clase. 
+    // var respuesta = [] ;                                      //# busca en el id del elemento html no de la clase. 
     function personasAlcanzadas(){
         
         let poblacion = 
@@ -35,22 +35,13 @@ $(document).ready(function() {
                 }
             },
             dataType: "json"
-        });
-
-        /** Esta parte... no tengo muy claro lo que hace. Dice algo asi:
-         * 
-         * Cuando la variable request se procesa correctamente, si devuelve un error = a 0... ??
-         *      y si devuleve un error message, imprimelo en una ventana emergente para informar. (que es lo que me pasa amenudo)
-         * 
-         * 
-         * Si le peticion falla, imprime un mensaje en una ventana emergente con un mensaje 
-         * 
-         * */ 
+        }); 
         
 
         request.done(function (response) {
            
-            let regiones_de_salud = {};
+            
+            let regiones_de_salud = {}; // seria regiones_de_salud o region_de_salud??
             let poblaciones = {};
             poblaciones["TSF"] = {
                 backgroundColor: "#ff0000",
@@ -67,29 +58,30 @@ $(document).ready(function() {
                 borderColor: "#000",
                 borderWidth:1
             };
+
             nombre_regiones = [];
+            response.forEach(function(){ 
+                poblaciones[response.poblacion].region_de_salud = response.Total_de_Personas_Alcanzadas;
+                nombre_regiones.push(response.region_de_salud);
 
-           response.forEach(function(resultado){ 
-                poblaciones[resultado.poblacion][region_de_salud] = resultado.Total_de_Personas_Alcanzadas;
-                nombre_regiones.push(resultado.region_de_salud);
+                if (!response.region_de_salud in regiones_de_salud){
+                    // Si la region no existe, la creamos, y ponemos sus poblaciones a 0
+                    regiones_de_salud[response.region_de_salud] = poblaciones;
+                    nombre_regiones.push(response.region_de_salud);
+                }
 
-                // if (!resultado.region_de_salud in regiones_de_salud){
-                //     // Si la region no existe, la creamos, y ponemos sus poblaciones a 0
-                //     regiones_de_salud[resultado.region_de_salud] = poblaciones;
-                //     nombre_regiones.push(resultado.region_de_salud);
-                // }
-
-                // regiones_de_salud[resultado.region_de_salud][resultado.poblacion] = resultado.Total_de_Personas_Alcanzadas;                
+                regiones_de_salud[response.region_de_salud][response.poblacion] = response.Total_de_Personas_Alcanzadas;                
             });
 
             dataset = [];
-            poblaciones.forEach(function(poblacion, idx){
+            arrayPoblaciones = Object.entries(poblaciones);
+            arrayPoblaciones.forEach(function (poblaciones, Total_de_Personas_Alcanzadas){
                 data = {
-                    label: idx,
+                    label: poblaciones,
                     // recorrer el array de nombre regiones para generar un array con el número de alcanzados de dichas regiones
                     // para esta poblacion
-                    data: nombre_regiones.map(function(reg){
-                        return poblacion.reg;
+                    data: nombre_regiones.map(function(Total_de_Personas_Alcanzadas){
+                        return poblacion.Total_de_Personas_Alcanzadas;
                     }),
                     backgroundColor: "#ff0000",
                     borderWidth:1,
@@ -99,11 +91,7 @@ $(document).ready(function() {
             });
 
 
-            ChartIt(regiones_de_salud, nombre_regiones);
-
-            function ChartIt(datos){
-
-                    dividir_objeto();
+            function ChartIt(){
 
                     const ctx = document.getElementById("canvas");
                     let myChart = new Chart(ctx, {
@@ -128,6 +116,8 @@ $(document).ready(function() {
                     }});
             }
 
+            ChartIt(regiones_de_salud, nombre_regiones);
+
 
         }); 
     
@@ -135,7 +125,9 @@ $(document).ready(function() {
         request.fail(function (jqXHR, textStatus) {
             alert("Ocurrió un error: " + textStatus);          // para reflejar los errores que no son controlados en el script, como errores de conexion
         });
-    };
+
+    }
+    
 
     $("#enviar").on('click', personasAlcanzadas);
 
